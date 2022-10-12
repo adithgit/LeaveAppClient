5f183572d7280bc2e8a8c53c7d6fcc9e7496c39f
adithgit <5285@mbcpeermade.com>
2022-10-12 20:49:01 +0530

    vercel push

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Api = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: 'https://minimaleave.herokuapp.com/api',
     withCredentials: true,
     headers:{
         'Content-type':'application/x-www-form-urlencoded'
